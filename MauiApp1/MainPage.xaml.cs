d7c629a8749eeb509524f1a3f37fc6422fbb154f
Hostile Cat <hjohanalejandro@gmail.com>
2024-07-23 09:56:46 -0500

    Agregar archivos de proyecto.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Button Clicked", "You clicked the button!", "OK");
+        }
+    }
+
+}
